15a40f3dd40c760a7635d68ed34e1c161c810a07
Irina Velychko <76870532+IrinaV713@users.noreply.github.com>
2022-05-21 22:23:15 +0300

    Add insert(), delete(), get()

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -48,4 +48,91 @@ public class DoublyLinkedList {
         }
         return len;
     }
+
+    public void insert(char c, int index){
+        if(index < 0 || index > this.length()){
+            throw new IndexOutOfBoundsException("Error. Invalid index");
+        }
+        else{
+            Node newNode = new Node();
+            newNode.data = c;
+            newNode.next = null;
+            newNode.prev = null;
+
+            if(index == 0){
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            else if(index == this.length()){
+                newNode.prev = tail;
+                tail.next = newNode;
+                tail = newNode;
+            }
+            else{
+                Node temp = head;
+                for(int i = 1; i < index; i++) {
+                    if(temp != null) {
+                        temp = temp.next;
+                    }
+                }
+                if(temp != null) {
+                    newNode.next = temp.next;
+                    newNode.prev = temp;
+                    temp.next = newNode;
+                    if(newNode.next != null) newNode.next.prev = newNode;
+                }
+            }
+        }
+    }
+
+    public char delete(int index){
+        char result;
+        if(index < 0 || index >= this.length() || head == null){
+            throw new IndexOutOfBoundsException("Error. Invalid index");
+        }
+        else if(index == 0){
+            result = this.get(index);
+            head = head.next;
+            if (head != null) head.prev = null;
+        }
+        else{
+            result = this.get(index);
+            Node temp = head;
+            for(int i = 1; i < index; i++) {
+                if(temp != null) {
+                    temp = temp.next;
+                }
+            }
+            if(temp != null && temp.next != null) {
+                temp.next = temp.next.next;
+                if(temp.next != null) {
+                    if (temp.next.next != null) temp.next.next.prev = temp.next;
+                }
+            }
+        }
+        return result;
+    }
+
+    public char get(int index){
+        if(index < 0 || index >= this.length()){
+            throw new IndexOutOfBoundsException("Error. Invalid index");
+        }
+        else{
+            Node elem = head;
+            char result = Character.MIN_VALUE;
+            int i = 0;
+            if(head != null) {
+                while(elem != null) {
+                    if(i == index){
+                        result = elem.data;
+                        break;
+                    }
+                    i++;
+                    elem = elem.next;
+                }
+            }
+            return result;
+        }
+    }
 }
--- src/test/java/DoublyLinkedListTest.java
+++ src/test/java/DoublyLinkedListTest.java
@@ -31,4 +31,53 @@ public class DoublyLinkedListTest {
         dll.append('5');
         assertEquals(5, dll.length());
     }
+
+    @Test
+    void insert() {
+        dll.append('a');
+        dll.append('b');
+        dll.append('c');
+        dll.insert('q', 1);
+        assertEquals('q', dll.head.next.data);
+        dll.insert('k', 0);
+        assertEquals('k', dll.head.data);
+        dll.insert('o', 5);
+        assertEquals('o', dll.tail.data);
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.insert('w', 8));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.insert('j',-2));
+    }
+
+    @Test
+    void delete() {
+        dll.append('a');
+        dll.append('b');
+        dll.append('c');
+        dll.append('d');
+        assertEquals('b', dll.delete(1));
+        assertEquals(3, dll.length());
+        assertEquals('d', dll.delete(2));
+        assertEquals(2, dll.length());
+        assertEquals('a', dll.delete(0));
+        assertEquals(1, dll.length());
+        assertEquals('c', dll.head.data);
+        assertEquals('c', dll.delete(0));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.delete(0));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.delete(3));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.delete(-1));
+
+    }
+
+    @Test
+    void get() {
+        dll.append('a');
+        dll.append('b');
+        dll.append('c');
+        dll.append('d');
+        assertEquals('c', dll.get(2));
+        assertEquals('a', dll.get(0));
+        assertEquals('d', dll.get(3));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.get(4));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.get(-1));
+        assertThrows(IndexOutOfBoundsException.class, ()-> dll.get(7));
+    }
 }
